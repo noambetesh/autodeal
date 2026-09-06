c3d426092aa8ae7464a09e71e1d8ca970de187f1
noambetesh <148471415+noambetesh@users.noreply.github.com>
2026-09-06 17:01:32 +0300

    Add PWA service worker

---
+++ app/sw.js
@@ -0,0 +1,5 @@
+const CACHE='autodeal-v1';
+const ASSETS=['./','./index.html','./app.js','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
